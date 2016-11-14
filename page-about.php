08b2827b464086d22c97c47532c760ba1856e306
whencespence <spencer.christina@gmail.com>
2016-11-13 23:15:54 -0500

    front page media queries

--- page-about.php
+++ page-about.php
@@ -13,30 +13,32 @@
 			<p class="intro_sub"><?php the_field('introductory_sub_header'); ?></p>
 			<p class="body_header"><?php the_field('body_header'); ?></p>
 
-		<!-- about container -->
-			<div class="about_container">
+			<div class="about_container flex_container">
 
 				<div class="about_content">
 					<p class="about_body_content"><?php the_field('body_content'); ?></p>
-				</div>
-				
-				<div class="outer_container flex_container">
-					<div class="staff_container flex_container">
-						<?php if( have_rows('staff_content') ): while( have_rows('staff_content') ) : the_row(); ?>
-							<div class="staff">
-								<?php $image = get_sub_field('staff_image'); ?>
-								<img src="<?php echo $image['url'] ?>" class= "staff_image" alt="<?php echo $image['alt']?>">
-								<p class="staff_name"><?php the_sub_field('staff_name'); ?></p>
-							</div>
-						<?php endwhile; endif; ?>
+					<div class="inner_container flex_container">
+						<div class="staff_container flex_container">
+							<?php if( have_rows('staff_content') ): while( have_rows('staff_content') ) : the_row(); ?>
+								<div class="staff">
+									<?php $image = get_sub_field('staff_image'); ?>
+									<img src="<?php echo $image['url'] ?>" class= "staff_image" alt="<?php echo $image['alt']?>">
+									<p class="staff_name"><?php the_sub_field('staff_name'); ?></p>
+								</div>
+							<?php endwhile; endif; ?>
+						</div>
 					</div>
-					<div class="about_image">
+				</div>
+
+				<div class="about_image">
+					<div class="inner_about_image">
 						<?php $image = get_field('about_image'); ?>
 						<img src="<?php echo $image['url'] ?>" class= "about_image" alt="<?php echo $image['alt']?>">
 					</div>
 				</div>
 
-			</div><!-- end of about_container -->
+			</div>
+
 		</div><!-- end of wrapper -->
 
      <?php endwhile; // end the loop?>
